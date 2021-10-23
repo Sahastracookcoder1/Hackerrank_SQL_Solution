5fcc406b2ea7c86c8daf69a8b42fa46fdff76bde
Ankit Raj <44525721+ankit039@users.noreply.github.com>
2021-10-23 11:24:17 +0530

    Create Diagonal-Difference.cpp

---
+++ Diagonal-Difference.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {  
+    int N; 
+    cin >> N; 
+    
+    int i, j; 
+    
+    int sumdiag1 = 0; 
+    int sumdiag2 = 0; 
+    for(i = 0; i < N; i++){
+        for(j = 0; j< N; j++)
+        {
+           int no; 
+           cin >> no; 
+           if(i == j)
+               sumdiag1 += no; 
+           if(i+j == N-1)
+              sumdiag2 += no; 
+        }
+    }
+    
+    cout << abs(sumdiag1 - sumdiag2);
+    return 0;
+}
